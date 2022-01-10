f5f7283adbacbffa97a28045ee6b0480d85d8ade
Jonathan Cheng <jonathanmcheng@gmail.com>
2022-01-10 16:13:30 -0500

    Create assertEqual function, similar to console.assert

---
+++ assertEqual.js
@@ -0,0 +1,7 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("⛔⛔⛔ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
