e58cf922c95093f49e3676faf3c9d6ff6ab101c8
Jonathan Cheng <jonathanmcheng@gmail.com>
2022-01-11 16:50:13 -0500

    Committing eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+// assertEqual function
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}.`);
+  } else {
+    console.log(`⛔ Assertion Failed: ${actual} !== ${expected}.`);
+  }
+};
+
+const eqArrays = (firstArray, secondArray) => {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  } else {
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
+    } return true;
+  }
+};
