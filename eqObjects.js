879779ff4aee447c40933a7c570738575959dc9d
Jonathan Cheng <jonathanmcheng@gmail.com>
2022-01-12 17:16:06 -0500

    refactored to check lengths of Object.keys in the beginning

--- eqObjects.js
+++ eqObjects.js
@@ -11,18 +11,14 @@ const assertEqual = (actual, expected) => {
 // Function
 const eqObjects = (object1, object2) => {
   let keyArray1 = Object.keys(object1);
-  let valueArray1 = Object.values(object1);
   let keyArray2 = Object.keys(object2);
-  let valueArray2 = Object.values(object2);
+  if (keyArray1.length !== keyArray2.length) {
+    return false;
+  }
+  let valueArray1 = Object.values(object1);
   // Loop through object1's keys and values in the arrays
   for (let i = 0; i < keyArray1.length; i++) {
     if (valueArray1[i] !== object2[keyArray1[i]]) {
-      return false;
-    }
-  }
-  // Loop through object2's keys and values in arrays
-  for (let i = 0; i < keyArray2.length; i++) {
-    if (valueArray2[i] !== object1[keyArray2[i]]) {
       return false;
     }
   } return true;
