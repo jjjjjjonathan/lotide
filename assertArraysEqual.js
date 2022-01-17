a942bc71fd0565cdb457c822b037c8469f96c0cd
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-01-17 16:42:56 -0500

    added a require for eqArrays to assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = (actual, expected) => {
-  if (actual.length !== expected.length) {
-    return false;
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        return false;
-      }
-    } return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (firstArray, secondArray) => {
   if (eqArrays(firstArray, secondArray)) {
