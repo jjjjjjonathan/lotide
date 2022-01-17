1aab5527866bbe0279ad6cb5eaac4811fe233e74
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-01-17 17:01:27 -0500

    installed mocha and chai, and coverted tests for head function to use mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,11 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const array = [1, 2, 3];
-const result = head(array);
-
-assertEqual(result, 1)
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
