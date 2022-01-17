18cb5343897828172a4c2768febb808056501cd3
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-01-17 17:25:10 -0500

    implemented mocha and chai tests for middle and tail functions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,13 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
+describe('#middle', () => {
+  it(`should return [2] for [1, 2, 3]`, () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it(`should return ["lewis", "hamilton"] for ["44", "lewis", "hamilton", 44]`, () => {
+    assert.deepEqual(middle(["44", "lewis", "hamilton", 44]), ["lewis", "hamilton"]);
+  });
+});
 assertArraysEqual(middle([1, 2, 3]), [2]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,14 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
 
-const arr = [1, 2, 3];
-
-assertArraysEqual(tail(arr), [2, 3])
+describe("#tail", () => {
+  it('should return [2, 3] for [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it(`should return ["go", "raptors"] for ["lets", "go", "raptors"]`, () => {
+    assert.deepEqual(tail(["lets", "go", "raptors"]), ["go", "raptors"]);
+  });
+  it(`should return [] for an array with just one item`, () => {
+    assert.deepEqual(tail([1]), []);
+  });
+});
